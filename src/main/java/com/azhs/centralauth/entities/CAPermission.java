eb0ec8529f6f8b453e330cfafaf6e79d90bb7826
Azhar Hussnain <azhar.hussnain>
2024-01-17 15:05:09 +0500

    add-entiti-role-permission

---
+++ src/main/java/com/azhs/centralauth/entities/CAPermission.java
@@ -0,0 +1,22 @@
+package com.azhs.centralauth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Document(collection = "permissions")
+@Data
+public class CAPermission {
+    @Id
+    private String id;
+
+    private String name;
+
+    @DocumentReference(lazy=true)
+    private CARole caRole;
+}
--- src/main/java/com/azhs/centralauth/entities/CARole.java
+++ src/main/java/com/azhs/centralauth/entities/CARole.java
@@ -2,30 +2,29 @@ package com.azhs.centralauth.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.ReadOnlyProperty;
-import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.security.Permission;
 import java.util.List;
 
-@Data
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Document(collection = "users")
-public class User {
+@Data
+@Document(collection = "roles")
+public class CARole {
     @Id
     private String id;
-    private String firstName;
-    private String lastName;
-    @Indexed(unique = true)
-    private String email;
+    private String name;
+
+    @DocumentReference(lazy=true)
+    private User user;
 
     @ReadOnlyProperty
-    @DocumentReference(lookup="{'user':?#{#self._id} }")
-    private List<CARole> caRole;
+    @DocumentReference(lookup="{'caRole':?#{#self._id} }")
+    private List<CAPermission> caPermissions;
 }
